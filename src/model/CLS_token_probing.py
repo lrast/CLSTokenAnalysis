Random readout learning: fixed classifier freezing and hidden state detach. Uses torch checkpointing to save memory on gradients. Temperature annealing is important for learning.

src/model/CLS_token_probing.py
@@ -4,6 +4,7 @@ import torch
 import pytorch_lightning as pl
 
 from torch import nn
+from torch.utils.checkpoint import checkpoint
 from huggingface_hub import PyTorchModelHubMixin
 
 
@@ -15,7 +16,7 @@ class SimpleReadOutAttachment(pl.LightningModule, PyTorchModelHubMixin):
         super().__init__()
         self.layer_ind = layer_ind
         self.decoder = ModuleSpecificDecoder(**decoderKwargs)
-        self.classification_loss = torch.nn.CrossEntropyLoss()
+        self.classification_loss = nn.CrossEntropyLoss()
 
         if not train_probe:
             for parameter in self.decoder.probe.parameters():
@@ -25,8 +26,8 @@ class SimpleReadOutAttachment(pl.LightningModule, PyTorchModelHubMixin):
         self.internal_loss_weight = internal_loss_weight
         self.base_layer = parent_layers[self.layer_ind]
 
-    def forward(self, hidden_states, labels=None):
-        model_input = hidden_states[self.layer_ind]
+    def forward(self, outputs, labels=None):
+        model_input = outputs.hidden_states[self.layer_ind].detach()
         readouts = self.decoder.forward(model_input, self.base_layer)
 
         if labels is None:
@@ -44,13 +45,117 @@ class SimpleReadOutAttachment(pl.LightningModule, PyTorchModelHubMixin):
         return {'decoder.' + k: v for k, v in decoder_dict.items()}
 
 
-class SelfCalibratingReadout(pl.LightningModule, PyTorchModelHubMixin):
-    """Class in charge of attaching internal readouts to the backbone model
-    Addition loss for mis-calibration in predictions
-
-    TODO: Needs to be implemented 
+class SecondaryReadouts(SimpleReadOutAttachment):
+    """ Secondary readout: trained to produce readouts similary to parent logits
+    This is a simplified version of the self-calibrating readouts to ensure that
+    the KL training doesn't chase itself.
     """
-    pass
+    def __init__(self, layer_ind, train_probe=True,
+                 num_samples=64, temperature=1.0, return_calibration_loss=False,
+                 **decoderKwargs):
+        super().__init__(layer_ind, train_probe, **decoderKwargs)
+        self.self_calibration_loss = nn.KLDivLoss(reduction='batchmean')
+        self.num_samples = num_samples
+        self.return_calibration_loss = return_calibration_loss
+        self.temperature = temperature
+        self.annealing_rate = (1-1/4000)
+        self.save_hyperparameters()
+
+    def update_temperature(self):
+        """ Temperature annealing every step """
+        self.temperature = max(0.1, self.temperature * self.annealing_rate)
+
+    def forward(self, outputs, labels=None):
+        model_input = outputs.hidden_states[self.layer_ind].detach()
+        parent_logits = outputs.logits.detach()
+
+        self.update_temperature()
+
+        # Sample multiple readouts.
+        # Uses checkpointing when training to prevent the memory graphs from blowing up
+        sampled_readouts = torch.stack([checkpoint(self.decoder.forward, model_input,
+                                                   self.base_layer, use_reentrant=False,
+                                                   preserve_rng_state=True)
+                                        for i in range(self.num_samples)
+                                        ])
+
+        # Self-calibration loss:
+        # note: targets are 'frequencies', predictions are logits
+        softmax_readouts = torch.softmax(sampled_readouts / self.temperature, dim=-1
+                                         ).mean(dim=0).clamp(1E-30)
+
+        #log_softmaxes = torch.log_softmax(sampled_readouts, dim=-1)  # [N, classes]
+        #log_mixture = torch.logsumexp(log_softmaxes, dim=0) - torch.log(torch.tensor(self.num_samples))
+
+        self_calibration_loss = self.self_calibration_loss(
+                                        torch.log_softmax(parent_logits, dim=-1),
+                                        softmax_readouts
+                                        )
+
+        # logging custom
+        shared_mass = (softmax_readouts * torch.softmax(parent_logits, dim=-1)
+                       ).sum(1).mean(0).item()
+
+        self.to_log = {'shared_mass': shared_mass, }
+
+        # using the self-calibration loss in later training.
+        return self.internal_loss_weight * self_calibration_loss, softmax_readouts
+
+
+class SelfCalibratingReadout(SimpleReadOutAttachment):
+    """Class in charge of attaching internal readouts to the backbone model
+
+    Addition loss for mis-calibration in predictions: aim to produce random answers that
+    1. Average to logit predictions
+    2. In soft-max, also average to logits!
+    """
+    def __init__(self, layer_ind, train_probe=True,
+                 num_samples=64, temperature=1.0, return_calibration_loss=False,
+                 **decoderKwargs):
+        super().__init__(layer_ind, train_probe, **decoderKwargs)
+        self.self_calibration_loss = nn.KLDivLoss(reduction='batchmean')
+        self.num_samples = num_samples
+        self.return_calibration_loss = return_calibration_loss
+        self.temperature = temperature
+        self.save_hyperparameters()
+
+    def forward(self, outputs, labels=None):
+        model_input = outputs.hidden_states[self.layer_ind].detach()
+
+        # Sample multiple readouts.
+        # Uses checkpointing when training to prevent the memory graphs from blowing up
+        sampled_readouts = torch.stack([checkpoint(self.decoder.forward, model_input,
+                                                   self.base_layer, use_reentrant=False,
+                                                   preserve_rng_state=True)
+                                        for i in range(self.num_samples)
+                                        ])
+
+        mean_readouts = sampled_readouts.mean(dim=0)
+
+        if labels is None and not self.return_calibration_loss:
+            return mean_readouts
+
+        # Self-calibration loss:
+        # note: targets are 'frequencies', predictions are logits
+        softmax_readouts = torch.softmax(sampled_readouts / self.temperature, dim=-1
+                                         ).mean(dim=0)
+        self_calibration_loss = self.self_calibration_loss(
+                                        torch.log_softmax(mean_readouts, dim=-1).clamp(min=-30),
+                                        softmax_readouts
+                                        )
+
+        # using the self-calibration loss in later training.
+        if labels is None:
+            return self.internal_loss_weight * self_calibration_loss, mean_readouts
+
+        classification_loss = self.classification_loss(mean_readouts, labels)
+
+        total_loss = classification_loss + self.internal_loss_weight * self_calibration_loss
+
+        # logging the classification loss here.
+        self.to_log = {'classification_loss': classification_loss.item()}
+
+        return total_loss, mean_readouts
 
 
 class ModuleSpecificDecoder(pl.LightningModule, PyTorchModelHubMixin):
@@ -174,6 +279,7 @@ class CLSGenerator(pl.LightningModule):
         input_dim = sample_dim
         for i in range(num_layers):
             layers.append(nn.Linear(input_dim, hidden_dim))
+            #layers.append(nn.BatchNorm1d(hidden_dim))
             layers.append(nn.LeakyReLU())
             input_dim = hidden_dim
         layers.append(nn.Linear(hidden_dim, output_dim))

src/model/harness.py
@@ -10,6 +10,8 @@ from src.utilities import HookContext
 from torch.optim.lr_scheduler import OneCycleLR
 from dataclasses import dataclass
 
+from lightning.pytorch.utilities import grad_norm
+
 
 @dataclass
 class TrainConfig:
@@ -23,6 +25,7 @@ class TrainConfig:
 
     save_readout_only: bool = True
     readout_loss_weight: float = 0.0
+    gradient_clip_val: float = None
 
 
 class ModelWrapper(pl.LightningModule):
@@ -108,7 +111,7 @@ class ModelWrapper(pl.LightningModule):
                 return outs
         else:
             base_outs = self.model(x, output_hidden_states=True)
-            return self.readout.forward(base_outs.hidden_states, labels=labels)
+            return self.readout.forward(base_outs, labels=labels)
 
     def training_step(self, batch, batch_idx):
         """
@@ -119,7 +122,14 @@ class ModelWrapper(pl.LightningModule):
 
         loss, outs = self.forward(inputs, labels=labels)
 
+        # utility for additional logging from the readout.
+        if self.readout is not None and hasattr(self.readout, 'to_log'):
+            self.log_dict({f'train/{key}': value for key, value in
+                          self.readout.to_log.items()})
+
         self.log('train/loss', loss)
+        self.log('debug/min', outs.min().item())
+        self.log('debug/max', outs.max().item())
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -146,12 +156,17 @@ class ModelWrapper(pl.LightningModule):
         self.log('test/accuracy', acc)
         return {'test_accuracy': acc}
 
+    def on_before_optimizer_step(self, optimizer):
+        """debug: gradient norm logging"""
+        norms = grad_norm(self, norm_type='inf')
+        self.log_dict(norms)
+
     def configure_optimizers(self):
         """Adam with cosine scheduling
         """
         lr = self.train_cfg.max_lr
 
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr / 25.)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=lr / 25.)
 
         scheduler = OneCycleLR(optimizer, epochs=self.train_cfg.epochs,
                                steps_per_epoch=self.train_cfg.steps_per_epoch,
@@ -215,12 +230,13 @@ class ModelWrapper(pl.LightningModule):
 
     def freeze_backbone(self, freeze_classifier=False):
         """ freeze the backbone a put it into eval mode """
-        for param in self.model.parameters():
-            param.requires_grad = False
-
-        if not freeze_classifier:
-            for params in self.get_classifier_module().parameters():
-                params.requires_grad = True
+        for name, param in self.model.named_parameters():
+            if ((not freeze_classifier) and
+               (name.split('.')[0] == self.CLS_classifier_name)):
+                # don't change anything inside the classifier
+                pass
+            else:
+                param.requires_grad = False
 
     def unfreeze_backbone(self):
         for param in self.model.parameters():